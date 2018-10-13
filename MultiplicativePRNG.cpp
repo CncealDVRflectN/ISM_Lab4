eb7024e8c9048b651adeb91b6d1a8512aa409a96
TTOJlTOPA <zhuk.valeriy.98@gmail.com>
2018-10-13 22:04:40 +0300

    Initial commit

---
+++ MultiplicativePRNG.cpp
@@ -0,0 +1,35 @@
+﻿#include "MultiplicativePRNG.h"
+
+MultiplicativePRNG::MultiplicativePRNG(long long module, long long seed, int multiplier) : module(module), multiplier(multiplier), seed(seed)
+{
+	this->last = seed;
+}
+
+MultiplicativePRNG::MultiplicativePRNG(const MultiplicativePRNG* source) : module(source->module), multiplier(source->multiplier), seed(source->seed)
+{
+	this->last = source->last;
+}
+
+MultiplicativePRNG::~MultiplicativePRNG()
+= default;
+
+double MultiplicativePRNG::next() const
+{
+	last = (last * multiplier) % module;
+	return (double)last / module;
+}
+
+double MultiplicativePRNG::next(double from, double to) const
+{
+	return from + (to - from) * next();
+}
+
+void MultiplicativePRNG::reset() const
+{
+	last = seed;
+}
+
+MultiplicativePRNG* MultiplicativePRNG::clone() const
+{
+	return new MultiplicativePRNG(this);
+}
---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main() {
+    const int pointsNum = 10;
+    
+    return 0;
+}
